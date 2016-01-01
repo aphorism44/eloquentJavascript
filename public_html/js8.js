ad8e1ff83aba1543a5cc758de44b3752b2d78a3a
Dominic Jesse <d-jesse@comcast.net>
2015-12-31 21:03:14 -0600

    start ch 8 exercises

--- public_html/js8.js
+++ public_html/js8.js
@@ -1,5 +1,33 @@
 
 "use strict";
 
-for (c = 0; c< 10; c++)
-    console.log(c);
+/*Retry*/
+
+function primitiveMultiply(a, b) {
+    var check = Math.random();
+    try { 
+        if (check <= 0.5) {
+            console.log(a * b);
+            return a * b;
+        } else {
+            throw new MuliplicatorUnitFailure("Invalid multiplication for : " + a + ", " + b) ;
+        }
+    } catch(e) {
+        if (e instanceof MuliplicatorUnitFailure)
+            console.log("failed: " + e);
+        else
+            throw e;
+    }
+}
+
+function MuliplicatorUnitFailure(message) {
+  this.message = message;
+  this.stack = (new Error()).stack;
+}
+MuliplicatorUnitFailure.prototype = Object.create(Error.prototype);
+MuliplicatorUnitFailure.prototype.name = "MultiplicatorUnitFailure";
+
+while (primitiveMultiply(3,4) instanceof MuliplicatorUnitFailure) {
+    console.log(primitiveMultiply(3,4));
+}
+
