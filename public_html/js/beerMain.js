29250abef4d8cfa51f2250f1fa7dd88add50ab22
u958jesse <u958jesse@BUCHDJESSEW7.bsus.com>
2016-02-29 11:58:52 -0600

    move MVC/supply chain work to different repository

--- public_html/js/beerMain.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-require(['Models/Game', 'json!Data/data.json']
-    , function(Game, InitialData){
-        
-        var game = new Game(InitialData.facilityData);
-        game.printInformation();
-        
-});
